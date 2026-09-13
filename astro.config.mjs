261057bbfd09da6e5de5cf9df5dfca2fce699448
Marvideo <marvideomc.pro@gmail.com>
2026-09-13 16:45:11 +0200

    add base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [react()],
 
-  site: "https://marvideo2009.github.io/Lyse-AI-Community.github.io/",
+  site: "https://marvideo2009.github.io",
+  base: "/Lyse-AI-Community.github.io",
 
   vite: {
     plugins: [tailwindcss()]
