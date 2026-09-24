a8b998878bae21219c479ef2fd144980a831a692
Baptiste Pruski <baptiste.pruski@laposte.net>
2026-09-24 16:09:31 +0200

    Added of the website on github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
 
-  site: "https://sparkslyse-community.github.io/",
+  // URL de base de ton compte GitHub
+  site: 'https://arthurguerrier.github.io',
+  
+  // NOM EXACT DU REPO (obligatoire !)
+  base: '/SparksLyse-Community.github.io/',
 
   vite: {
     plugins: [tailwindcss()]
